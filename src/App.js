e34b277ff8cd5c33a83e80b680927e9e78470649
choiheesu <dqdq4197@naver.com>
2019-08-21 18:32:35 +0900

    movie_app_style_update

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
 
   getMovies = async ()=> {
     const {data: {data: {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    console.log({movies});
     this.setState({movies, isLoading:false});
   };
   componentDidMount(){
@@ -36,6 +37,7 @@ class App extends React.Component {
                 summary = {movie.summary}
                 poster = {movie.medium_cover_image}
                 genres = {movie.genres}
+                rating = {movie.rating}
               /> 
            ))}
         </div>
--- src/Movie.js
+++ src/Movie.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 
 
-function Movie({year, title, summary, poster, genres}) {
+function Movie({year, title, summary, poster, genres,rating}) {
+
+  const star=()=> {
+    const point= parseInt(rating);
+    const str=[];
+    for(let i=0; i<(point/2)-1;i++){
+      str[i]="★";
+    }
+    return str.join("");
+  };
     return (
       <div className="poster">
           <div className="imgwrap"> 
@@ -18,9 +27,11 @@ function Movie({year, title, summary, poster, genres}) {
                   </li>
               ))}
             </ul>
+            <h5>{
+              star()
+            }({rating})</h5>
             <p>{summary.slice(0,140)}...</p>
           </div>
-
       </div>  
     );
 }
